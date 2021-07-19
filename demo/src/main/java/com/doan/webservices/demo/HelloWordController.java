7b17b43b12890f585aa19a7c950ec90a44d6bae1
bendoan4366 <ben.doan4366@gmail.com>
2021-07-18 22:32:36 -0400

    added hello world controllers and demo bean controller

--- demo/src/main/java/com/doan/webservices/demo/HelloWordController.java
+++ demo/src/main/java/com/doan/webservices/demo/HelloWordController.java
@@ -1,2 +1,19 @@
-package com.doan.webservices.demo;public class HelloWordController {
+package com.doan.webservices.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWordController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "ay ay!";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("ay ay ay!", "Duke Deuce");
+    }
 }
--- demo/src/main/java/com/doan/webservices/demo/HelloWorldBean.java
+++ demo/src/main/java/com/doan/webservices/demo/HelloWorldBean.java
@@ -1,2 +1,40 @@
-package com.doan.webservices.demo;public class HellowWorldBean {
+package com.doan.webservices.demo;
+
+public class HelloWorldBean {
+
+    private String message;
+    private String rapper;
+
+    public HelloWorldBean() {
+    }
+
+    public HelloWorldBean(String message, String rapper) {
+        this.message = message;
+        this.rapper = rapper;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getRapper() {
+        return rapper;
+    }
+
+    public void setRapper(String rapper) {
+        this.rapper = rapper;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                ", rapper='" + rapper + '\'' +
+                '}';
+    }
+
 }
