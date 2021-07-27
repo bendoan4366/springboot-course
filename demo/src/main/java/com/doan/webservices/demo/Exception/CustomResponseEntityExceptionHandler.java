338b89cf0ddf2f7f254eed7c8adfb659d207eb8f
bendoan4366 <ben.doan4366@gmail.com>
2021-07-26 22:26:00 -0400

    added custom exception handling controller and messages

--- demo/src/main/java/com/doan/webservices/demo/Exception/CustomResponseEntityExceptionHandler.java
+++ demo/src/main/java/com/doan/webservices/demo/Exception/CustomResponseEntityExceptionHandler.java
@@ -1,2 +1,39 @@
-package com.doan.webservices.demo.Exception;public class CustomResponseEntityExceptionHandler {
+package com.doan.webservices.demo.Exception;
+
+import com.doan.webservices.demo.user.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.sql.Timestamp;
+
+//extend Spring Response entity handler to allow for custom response messages
+@ControllerAdvice //allows this controller shared across multiple @controller classes
+@RestController
+
+//used if you want to override the standard Entity reponse handler
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(Exception.class) // handle all exceptions - to be overwritten later
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request){
+        //instantiate exception response to be built into response entity
+        ExceptionResponse eResponse = new ExceptionResponse(new Timestamp(System.currentTimeMillis()), ex.getMessage(), request.getDescription(false));
+
+        //create and return new response entity in one step - will return only 500 internal server errors
+        return new ResponseEntity(eResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class) // handle UserNotFoundException
+    public final ResponseEntity<Object> handleUserNotFoundExceptions(UserNotFoundException ex, WebRequest request){
+        //instantiate exception response to be built into response entity
+        ExceptionResponse eResponse = new ExceptionResponse(new Timestamp(System.currentTimeMillis()), ex.getMessage(), request.getDescription(false));
+
+        //create and return new response entity in one step - will return only 500 internal server errors
+        return new ResponseEntity(eResponse, HttpStatus.NOT_FOUND);
+    }
+
 }
--- demo/src/main/java/com/doan/webservices/demo/Exception/ExceptionResponse.java
+++ demo/src/main/java/com/doan/webservices/demo/Exception/ExceptionResponse.java
@@ -1,2 +1,45 @@
-package com.doan.webservices.demo.Exception;public class ExceptionResponse {
+package com.doan.webservices.demo.Exception;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class ExceptionResponse {
+    //timestamp
+    //message
+    //detail
+
+    private Timestamp errorTimestamp;
+    private String errorMessage;
+    private String errorDetails;
+
+    public ExceptionResponse(Timestamp timestamp, String errorMessage, String errorDetails) {
+        super();
+        this.errorTimestamp = timestamp;
+        this.errorMessage = errorMessage;
+        this.errorDetails = errorDetails;
+    }
+
+    public Timestamp getErrorTimestamp() {
+        return errorTimestamp;
+    }
+
+    public void setErrorTimestamp(Timestamp errorTimestamp) {
+        this.errorTimestamp = errorTimestamp;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorDetails() {
+        return errorDetails;
+    }
+
+    public void setErrorDetails(String errorDetails) {
+        this.errorDetails = errorDetails;
+    }
 }
