3e87dd5b49f0fb52304f271e37c739653e25f48d
bendoan4366 <ben.doan4366@gmail.com>
2021-07-26 22:25:28 -0400

    added exception handling

--- demo/src/main/java/com/doan/webservices/demo/helloworld/HelloWordController.java
+++ demo/src/main/java/com/doan/webservices/demo/helloworld/HelloWordController.java
@@ -1,4 +1,4 @@
-package com.doan.webservices.demo;
+package com.doan.webservices.demo.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- demo/src/main/java/com/doan/webservices/demo/helloworld/HelloWorldBean.java
+++ demo/src/main/java/com/doan/webservices/demo/helloworld/HelloWorldBean.java
@@ -1,4 +1,4 @@
-package com.doan.webservices.demo;
+package com.doan.webservices.demo.helloworld;
 
 public class HelloWorldBean {
 
