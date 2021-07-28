cdb5b7cb5eccf548615666507ed6e43da2442ebe
bendoan4366 <ben.doan4366@gmail.com>
2021-07-28 15:24:05 -0400

    added spring cloud config server

---
+++ spring-cloud-config-server/src/test/java/com/doan/limitsmicroservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doan.limitsmicroservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
